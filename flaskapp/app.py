5834601d850cd1e0cfa392ef74d31b0770c9bf7b
harinivivek <harini.galaxytech@gmail.com>
2018-07-25 13:39:00 +0530

    Create UI for the above Article API's.
     - two screens, one for creating the articles and one for listing the articles.
     - user should be able to vote for a particular article and should be updated on the back end

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -71,7 +71,48 @@ def list():
     # sort the articles(list of dictionaries) in descending order of vote
     return {"articles": sorted(articles, key=lambda article: article["vote"], reverse=True)}, 200 
 
+@app.route("/article_ui/<string:article_id>/",methods=['GET', 'POST'])
+def article_ui(article_id):
+    message = ""
+    if request.method == "POST":
+        data = request.form
+        existing_article = _find_article(article_id)
 
+        if existing_article is not None:
+
+            message = "Article already created"
+            return render_template("article.html", message=message)
+
+
+        # set default value of 0 for integers(vote) and emtpy character for
+        # strings(title, content, author) in case user did not provide those values
+        new_article = {"article_id": article_id,
+                       "vote": data.get("vote", 0),
+                       "title": data.get("title", ""),
+                       "content": data.get("content", ""),
+                       "author": data.get("author", "")}
+
+        articles.append(new_article)
+
+        message = "Article created successfully"
+    return render_template("article.html", message=message)
+
+@app.route("/article_upvote_ui/<string:article_id>/",methods=['GET', 'POST'])
+def article_upvote_ui(article_id):
+    message = ""
+    if request.method == "POST":
+        data = request.form
+        vote = data.get("vote")
+        existing_article = _find_article(article_id)
+
+        if not vote or existing_article is None:
+
+            message = "Invalid vote or Article doesn't exist"
+            return render_template("article_upvote.html", message=message)
+
+
+        existing_article['vote'] = vote
+        message = "Article updated successfully"
 
 def get_article_data():
     # return articles list with each article as a list of fields to easily show in ui
@@ -84,6 +125,10 @@ def get_article_data():
         articles_data.append(field_list)
     return cols, articles_data
 
+@app.route("/article_list_ui",methods=['GET'])
+def article_list_ui():
+    table_column_headings, articles_data = get_article_data()
+    return render_template("article_list.html", cols=table_column_headings, articles=articles_data)
 
 
 app.run(port=5000)
